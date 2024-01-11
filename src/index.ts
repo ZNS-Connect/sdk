21c7b4bfe8dcd69a75934915c0422012aa801c4d
SyedAhkam <smahkam57@gmail.com>
2024-01-12 03:06:16 +0530

    feat: create TLD type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createZnsPublicClient } from './client';
 import { Chain } from './chain';
 import { znsPublicActions } from './actions';
+import TLD from './tld';
 
-export { createZnsPublicClient, Chain, znsPublicActions };
+export { createZnsPublicClient, Chain, znsPublicActions, TLD };
---
+++ src/tld.ts
@@ -0,0 +1,3 @@
+type TLD = 'zeta' | 'zero';
+
+export default TLD;
