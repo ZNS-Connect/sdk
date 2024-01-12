f8cb221a3592578ed7656af59859c4bf5d0ad346
SyedAhkam <smahkam57@gmail.com>
2024-01-12 22:15:06 +0530

    feat: update contract address for .zero tld

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -5,7 +5,7 @@ import ABI from './abi'; // NOTE: same ABI for all contracts
 const addresses: Record<Chain, Record<TLD, string>> = {
   [Chain.PolygonMumbai]: {
     [TLD.Zeta]: '0x3b7558d441824be63B797e919974897A1454D27b',
-    [TLD.Zero]: '0x5dEeDcCf0e5B4BbA5cA3cA0e213aB1aFfFfFfFfF', // FIXME: placeholder
+    [TLD.Zero]: '0xeD5fC231Fd428F44B602B77Ea7ECc53b2bB0f034',
   },
   [Chain.Polygon]: {
     [TLD.Zeta]: '0x5dEeDcCf0e5B4BbA5cA3cA0e213aB1aFfFfFfFfF', // FIXME: placeholder
